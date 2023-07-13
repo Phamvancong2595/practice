d6188c64f3c9d1b180426a94211db7c748445d88
Phamvancong2595 <congpham2595@gmail.com>
2023-07-13 21:02:32 +0700

    dsp iterator clean practice

--- bookstore/src/main/java/repository/Repository.java
+++ bookstore/src/main/java/repository/Repository.java
@@ -1,15 +1,43 @@
 package repository;
 
+import app.BookApplication;
+import com.tvd12.ezyfox.reflect.EzyGenerics;
+import command.IterableCommand;
+import command.SaveCommand;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
+@SuppressWarnings("unchecked")
 public interface Repository<E> {
-    void save(E entity) throws Exception;
-    void forEach(Consumer<E> consumer) throws Exception;
+    default void save(E entity) throws Exception {
+     final SaveCommand<E> command = BookApplication
+             .getInstance()
+             .getCommandProvider()
+             .provide(SaveCommand.class);
+     command
+             .entity(entity)
+             .execute();
+    }
+    default void forEach(Consumer<E> consumer) throws Exception {
+        final IterableCommand<E> command = BookApplication
+                .getInstance()
+                .getCommandProvider()
+                .provide(IterableCommand.class);
+        final Iterable<E> iterable = command
+                .entitiType(getEntityType)
+                .execute();
+        for (E entity: iterable) {
+            consumer.accept(entity);
+        }
+    }
     default List<E> findAll() throws Exception {
         final List<E> entities = new ArrayList<>();
         forEach(entities::add);
         return entities;
+    }
+
+    default Class<E> getEntityType() {
+        return EzyGenerics.getGenericInterfacesArguments(getClass().getInterfaces()[0], Repository.class,1)[0];
     }
 }
